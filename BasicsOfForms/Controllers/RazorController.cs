3c3db756d174dfb9978d7259ce21ec9a56777537
Dan Ventura <danwventura@icloud.com>
2016-11-01 18:42:38 -0500

    razor form created

---
+++ BasicsOfForms/Controllers/RazorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BasicsOfForms.Controllers
+{
+    public class RazorController : Controller
+    {
+        // GET: Razor
+        public ActionResult Razor()
+        {
+            return View();
+        }
+    }
+}
